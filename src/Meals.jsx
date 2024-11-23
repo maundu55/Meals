63d1ea05c6e2e0995cf9f9b05db957912ef012dc
maundu55 <marymaundu.mm@gmail.com>
2024-11-23 23:32:19 +0300

    fetch meals data using axios and useEffect

--- src/Meals.jsx
+++ src/Meals.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from "react"
+import './style.css'
 
 const Meals = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+      axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res)=>{
+        const items = res.data.meals
+        setItems(items)
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+
+  }, []);
+
+  const itemsList = items.map(({strMeal, strMealThumb, idMeal})=>{
+    return (
+      <section className="card">
+        <img src={strMealThumb} alt={strMeal} />
+
+        <section className="content">
+            <p>{strMeal}</p>
+            <p>{idMeal}</p>
+        </section>
+      </section>
+      
+    )
+  })
   return (
-    <div>
-        <h3>mary</h3>
+
+    <div className="items-container">
+       {itemsList}
     </div>
   )
 }
